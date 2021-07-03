70b518ada5618ae17f67f363eb4d8f7492984c11
iqbal khan <iqbal16697@gmail.com>
2021-07-03 11:44:05 +0530

    days 3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
-  final String name = "Pune";
+  final String pin = "01";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
-          child: Text("Welcom to Fladder Technology $name"),
+          child: Text(" Welcom to Fladder Technology Pune - $pin"),
         ),
       ),
-      drawer: Drawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: unused_local_variable
-       return MaterialApp(
+      return MaterialApp(
       home: HomePage(),
     );
   }
