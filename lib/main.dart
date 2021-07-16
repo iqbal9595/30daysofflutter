c63ce2b24a09d11ef88aa42b93cda606a76c555c
iqbal khan <iqbal16697@gmail.com>
2021-07-16 16:51:05 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_demo/pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -12,17 +12,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: unused_local_variable
     return MaterialApp(
-      
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-        ),
-        routes:{
-          "/" : (context)=>LoginPage(),
-          "/home" : (context)=>HomePage(),
-          "/login" : (context)=>LoginPage()
-        } ,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
-  }
+  }  
 }
